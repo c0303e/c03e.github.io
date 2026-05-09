094266796b19db0442d4ed874c4d1c03ee2471d1
xC03Ex <57694064+c0303e@users.noreply.github.com>
2026-05-08 23:19:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("RED TEAM OPERATOR LOADED");
+
+setInterval(() => {
+
+    document.title =
+    document.title === "C03E | Red Team Operator"
+    ? "ACCESS GRANTED"
+    : "C03E | Red Team Operator";
+
+}, 2000);
